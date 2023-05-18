56af1d8b06c915d306da1df62a795c1f6b4f8f46
Alex Pacheco <56262818+alexpacheko@users.noreply.github.com>
2023-05-19 00:13:02 +0200

    Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,6 +16,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule
 
   ],
   providers: [],
